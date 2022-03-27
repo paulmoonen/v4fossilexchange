b35d19cdd38b181c6801f5bfd1bacd4d9466051b
paulmoonen <paul_moonen@hotmail.com>
2022-03-27 15:12:28 +0200

    categoriespage looks good

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,18 +2,27 @@
 
 @section('content')
 
-<div class="categories">
-    <p>Our product Categories</p>
-    <ul>
-        <?php foreach ($categories as $category) : ?>
-            <li>
-                <a href="/category/<?= $category->id ?>"><?= $category->name ?></a>
-                                
-                <?= $category->description ?>
-            </li>
+<div class="container">
+    <div class="row">
+        <div class="categories col-6">
+            <p>Our product Categories</p>
+            <ul>
+                <?php foreach ($categories as $category) : ?>
+                    <li>
+                        <a href="/category/<?= $category->id ?>"><?= $category->name ?></a>
+                                        
+                        <?= $category->description ?>
+                    </li>
 
-        <?php endforeach ?>
-    </ul>
+                <?php endforeach ?>
+            </ul>
+        </div>
+
+        <div class="col-6">
+            <img src="/pictures/krijtrotsen.jpg" alt="white cliffs of le Treport" class="img-fluid">
+        </div>
+    </div>
+
 </div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <h1>empty main page</h1>
+    Visit our <a href="/category">products</a>
 
 </div>
 @endsection
