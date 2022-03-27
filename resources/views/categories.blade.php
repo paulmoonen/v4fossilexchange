931276900936efe164059a1c5f3d45cf8a8f17e3
paulmoonen <paul_moonen@hotmail.com>
2022-03-27 15:00:18 +0200

    .

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
 
-<div class="categories">
+<div class="categories row">
+    <div class="col-6">
     <p>Our product Categories</p>
-    <ul>
-        <?php foreach ($categories as $category) : ?>
-            <li>
-                <a href="/category/<?= $category->id ?>"><?= $category->name ?></a>
-                                
-                <?= $category->description ?>
-            </li>
+        <ul>
+            <?php foreach ($categories as $category) : ?>
+                <li>
+                    <a href="/category/<?= $category->id ?>"><?= $category->name ?></a>
+                                    
+                    <?= $category->description ?>
+                </li>
 
-        <?php endforeach ?>
-    </ul>
+            <?php endforeach ?>
+        </ul>
+    </div>
+    <div class="col-6">
+        <img src="/pictures/krijtrotsen.jpg" alt="white cliffs at le Tréport" class="img-fluid">
+    </div>
 </div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
 <div class="container">
-    <h1>empty main page</h1>
+    <div class="row">
+        <div class="col-4 categorieslist">
+            Browse our <a href="/category">product categories</a>
+
+        </div>
+        <div class="col-8">
+            <img src="/pictures/krijtrotsen.jpg" alt="white cliffs at le Tréport" class="img-fluid">
+
+
+        </div>
+
+
+    </div>
 
 </div>
 @endsection
