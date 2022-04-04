f27fc2f89cd20de334efa042c49a3add4bbca352
paulmoonen <paul_moonen@hotmail.com>
2022-04-04 13:47:10 +0200

    admin pages styled

--- resources/views/footer.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('footer')
-<div class="col-2"></div>
-<div class="col-8">
-    <h2 class="footer">Legal notice</h2>
-</div>
-<div class="col-2"></div>
-@endsection
